c351b52d2efd04cc67fad54766040a3d74434628
matias rivas <rivasleandromatias@gmail.com>
2013-11-15 02:18:13 -0300

    Fix

--- src/circularbuffer.c
+++ src/circularbuffer.c
@@ -1,15 +1,16 @@
 #include "../include/circularbuffer.h"
  
 /*
-** Fuente codigo CircularBuffer
-** http://en.wikipedia.org/wiki/Circular_buffer
+** Source: http://en.wikipedia.org/wiki/Circular_buffer
 */
+
+char cbuff[CB_SIZE];
 
 void cbInit(CircularBuffer *cb) {
     cb->size  = CB_SIZE; /* include empty elem */
     cb->start = 0;
     cb->end   = 0;
-    //cb->elems = (ElemType *)calloc(cb->size, sizeof(ElemType));
+    cb->elems = cbuff;
 }
  
 void cbFree(CircularBuffer *cb) {
@@ -24,7 +25,7 @@ int cbIsEmpty(CircularBuffer *cb) {
 /* Write an element, overwriting oldest element if buffer is full. App can
    choose to avoid the overwrite by checking cbIsFull(). */
 void cbWrite(CircularBuffer *cb, char *elem) {
-    *cb->elems[cb->end] = *elem;
+    cb->elems[cb->end] = *elem;
     cb->end = (cb->end + 1) % cb->size;
     if (cb->end == cb->start)
         cb->start = (cb->start + 1) % cb->size; /* full, overwrite */
@@ -32,6 +33,6 @@ void cbWrite(CircularBuffer *cb, char *elem) {
  
 /* Read oldest element. App must ensure !cbIsEmpty() first. */
 void cbRead(CircularBuffer *cb, char *elem) {
-    *elem = *cb->elems[cb->start];
+    *elem = cb->elems[cb->start];
     cb->start = (cb->start + 1) % cb->size;
 }
